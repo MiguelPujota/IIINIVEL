e5ef49cb043b0d466c37b2a2d7918fb6fb0d1f30
MiguelPujota <mickcomunicador9@gmail.com>
2025-06-08 17:29:46 -0500

    Add files via upload

---
+++ Proyectos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uea.edu.ec.proyectos;
+
+/**
+ *
+ * @author MICK
+ */
+public class Proyectos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
